c6ae3709e42e0a9a08b083a777ec409542400a23
Paulo Morais dos Santos <paulomdsbh@outlook.com>
2020-11-03 13:57:21 -0300

    Projeto foi criado!

---
+++ src/test/java/com/paulomdsbh/avanco/TestetecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulomdsbh.avanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestetecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
